8d931678103ef128436944cb5bc6baf312075995
Nazar <youtopak1@gmail.com>
2023-04-11 10:00:00 +0300

    add clearSearch

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -6,25 +6,45 @@ class SearchPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            term: ''
+            term: '',
+            clazz: 'btn_remove'
         }
     }
 
     onUpdataSearch = (e) => {
-        this.setState(({term}) => ({
-            term: e.target.value
+        this.setState(({term, clazz}) => ({
+            term: e.target.value,
+            clazz: `btn_remove ${e.target.value.length >= 1 ? 'active' : ''}`
         }))
 
         this.props.onUpdataTerm(e.target.value);
     }
 
+    onClearSearch = () => {
+        this.setState(({term, clazz}) => ({
+            term: '',
+            clazz: `btn_remove`
+        }))
+
+        this.props.onUpdataTerm('');
+    }
+
     render() {
         return (
-            <input type="text"
+            <div className='search-div'>
+                <input type="text"
                     className="form-control search-input"
                     placeholder="Найти сотрудника"
                     value={this.state.term} 
-                    onChange={this.onUpdataSearch}/>
+                    onChange={this.onUpdataSearch}
+                />
+
+                <button className={this.state.clazz}
+                    onClick={this.onClearSearch}  
+                >
+                    ✖
+                </button>
+            </div>
         )
     }
 }
